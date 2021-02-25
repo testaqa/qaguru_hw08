e0360eb8273b1c9da9dd4825f0ac827114024339
Vasiliy Golban <vgolban@accesssoftek.com>
2021-02-25 14:53:49 +0200

    Add allure, second test

--- src/test/java/tests/DemoQaTests.java
+++ src/test/java/tests/DemoQaTests.java
@@ -10,68 +10,125 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-public class DemoQaTests {
+public class DemoQaTests extends TestBase {
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+    String gender = "Other";
+    String mobile = faker.phoneNumber().subscriberNumber(10);
+    LocalDate birthday = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    String subjects = "Maths";
+    String hobby = "Reading";
+    String pictureFile = "test.png";
+    String address = faker.address().fullAddress();
+    String state = "Haryana";
+    String city = "Panipat";
 
     @Test
-    void AutomationPracticeFormTest() {
-        Faker faker = new Faker();
+    void SubmitForm() {
 
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-        String email = faker.internet().emailAddress();
-        String gender = "Other";
-        String mobile = faker.phoneNumber().subscriberNumber(10);
-        LocalDate birthday = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        String subjects = "Maths";
-        String hobby = "Reading";
-        String pictureFile = "test.png";
-        String address = faker.address().fullAddress();
-        String state = "Haryana";
-        String city = "Panipat";
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+        });
 
-        open("https://demoqa.com/automation-practice-form");
+        step("Fill the form", () -> {
+            $("#firstName").setValue(firstName);
 
-        $("#firstName").setValue(firstName);
+            $("#lastName").setValue(lastName);
 
-        $("#lastName").setValue(lastName);
+            $("#userEmail").setValue(email);
 
-        $("#userEmail").setValue(email);
+            $("input[name=gender][value=" + gender + "] ~ label").click();
 
-        $("input[name=gender][value=" + gender + "] ~ label").click();
+            $("#userNumber").setValue(mobile);
 
-        $("#userNumber").setValue(mobile);
+            $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            $("#dateOfBirthInput").sendKeys(birthday.format(DateTimeFormatter.ofPattern("dd LLLL yyyy")));
+            $("#dateOfBirthInput").pressEnter();
 
-        $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        $("#dateOfBirthInput").sendKeys(birthday.format(DateTimeFormatter.ofPattern("dd LLLL yyyy")));
-        $("#dateOfBirthInput").pressEnter();
+            $("#subjectsInput").setValue(subjects).pressEnter();
 
-        $("#subjectsInput").setValue(subjects).pressEnter();
+            $x("//label[contains(text(), '" + hobby + "')]").click();
 
-        $x("//label[contains(text(), '" + hobby + "')]").click();
+            $("#uploadPicture").uploadFromClasspath(pictureFile);
 
-        $("#uploadPicture").uploadFromClasspath(pictureFile);
+            $("#currentAddress").setValue(address);
 
-        $("#currentAddress").setValue(address);
+            $("#state input").setValue(state).pressEnter();
 
-        $("#state input").setValue(state).pressEnter();
+            $("#city input").setValue(city).pressEnter();
+        });
 
-        $("#city input").setValue(city).pressEnter();
-
-        $("#submit").click();
+        step("Click submit", () -> {
+            $("#submit").click();
+        });
 
         // Assert
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        step("Verify form is submitted", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        });
 
-        $x("//td[text()='Student Name']").sibling(0).shouldHave(text(firstName + " " + lastName));
-        $x("//td[text()='Student Email']").sibling(0).shouldHave(text(email));
-        $x("//td[text()='Gender']").sibling(0).shouldHave(text(gender));
-        $x("//td[text()='Mobile']").sibling(0).shouldHave(text(mobile));
-        $x("//td[text()='Date of Birth']").sibling(0).shouldHave(text(birthday.format(DateTimeFormatter.ofPattern("dd LLLL,yyyy"))));
-        $x("//td[text()='Subjects']").sibling(0).shouldHave(text(subjects));
-        $x("//td[text()='Hobbies']").sibling(0).shouldHave(text(hobby));
-        $x("//td[text()='Picture']").sibling(0).shouldHave(text(pictureFile));
-        $x("//td[text()='Address']").sibling(0).shouldHave(text(address));
-        $x("//td[text()='State and City']").sibling(0).shouldHave(text(state + " " + city));
+        step("Verify submitted data", () -> {
+            $x("//td[text()='Student Name']").sibling(0).shouldHave(text(firstName + " " + lastName));
+            $x("//td[text()='Student Email']").sibling(0).shouldHave(text(email));
+            $x("//td[text()='Gender']").sibling(0).shouldHave(text(gender));
+            $x("//td[text()='Mobile']").sibling(0).shouldHave(text(mobile));
+            $x("//td[text()='Date of Birth']").sibling(0).shouldHave(text(birthday.format(DateTimeFormatter.ofPattern("dd LLLL,yyyy"))));
+            $x("//td[text()='Subjects']").sibling(0).shouldHave(text(subjects));
+            $x("//td[text()='Hobbies']").sibling(0).shouldHave(text(hobby));
+            $x("//td[text()='Picture']").sibling(0).shouldHave(text(pictureFile));
+            $x("//td[text()='Address']").sibling(0).shouldHave(text(address));
+            $x("//td[text()='State and City']").sibling(0).shouldHave(text(state + " " + city));
+        });
     }
+
+    @Test
+    void SubmitFailedForm() {
+
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+        });
+
+        step("Fill the form", () -> {
+//            $("#firstName").setValue(firstName);
+
+            $("#lastName").setValue(lastName);
+
+            $("#userEmail").setValue(email);
+
+            $("input[name=gender][value=" + gender + "] ~ label").click();
+
+            $("#userNumber").setValue(mobile);
+
+            $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            $("#dateOfBirthInput").sendKeys(birthday.format(DateTimeFormatter.ofPattern("dd LLLL yyyy")));
+            $("#dateOfBirthInput").pressEnter();
+
+            $("#subjectsInput").setValue(subjects).pressEnter();
+
+            $x("//label[contains(text(), '" + hobby + "')]").click();
+
+            $("#uploadPicture").uploadFromClasspath(pictureFile);
+
+            $("#currentAddress").setValue(address);
+
+            $("#state input").setValue(state).pressEnter();
+
+            $("#city input").setValue(city).pressEnter();
+        });
+
+        step("Click submit", () -> {
+            $("#submit").click();
+        });
+
+        // Assert
+        step("Verify form is submitted", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        });
+    }
+
 }
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,47 @@
+package tests;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+//        Configuration.browser = System.getProperty("browser", "chrome");
+//        Configuration.startMaximized = true;
+//
+//        if(System.getProperty("remote_driver") != null) {
+//            // config for Java + Selenide
+//            DesiredCapabilities capabilities = new DesiredCapabilities();
+//            capabilities.setCapability("enableVNC", true);
+//            capabilities.setCapability("enableVideo", true);
+//            Configuration.browserCapabilities = capabilities;
+//            Configuration.remote = System.getProperty("remote_driver");
+//
+//            // config for Java + Selenium
+////        DesiredCapabilities capabilities = new DesiredCapabilities();
+////        capabilities.setCapability("browserName", "chrome");
+////        capabilities.setCapability("browserVersion", "87.0");
+////        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+////                "enableVNC", true,
+////                "enableVideo", true
+////        ));
+////        RemoteWebDriver driver = new RemoteWebDriver(
+////                URI.create("http://selenoid:4444/wd/hub").toURL(),
+////                capabilities
+////        );
+//        }
+    }
+
+//    @AfterEach
+//    public void afterEach() {
+//        attachScreenshot("Last screenshot");
+//        attachPageSource();
+//        attachAsText("Browser console logs", getConsoleLogs());
+//        if(System.getProperty("video_storage") != null)
+//            attachVideo();
+//        Selenide.closeWebDriver();
+//    }
+}
